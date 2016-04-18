132e90e6029a48dfaf5fcda3ad7c31df01521770
Remilia <HinanawiCirno@outlook.com>
2016-04-18 13:34:59 +0800

    Apollo-Parent

---
+++ Apollo-Spring/src/main/java/org/koumakan/apollo/spring/ioc/IoCMain.java
@@ -0,0 +1,13 @@
+package org.koumakan.apollo.spring.ioc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Desc
+ * @Author Remilia
+ * @Create 2016-04-18
+ */
+public class IoCMain {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IoCMain.class);
+}
